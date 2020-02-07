e1b7b5a31c3bbd0cd8e4b9ecfd36c019c2591ee9
Sergey Zakirov <erega74@gmail.com>
2020-02-08 01:13:50 +0500

    Сверстан блок 'footer' и добавлена адаптивность до минимальной ширины 320px

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+var burgerBtn = document.querySelector(".burger__btn");
+var headerList = document.querySelector(".header-content__link-list");
+var overlay = document.querySelector(".overlay");
+burgerBtn.addEventListener("click",  function() {
+    burgerBtn.classList.toggle("burger-cross__btn");
+    headerList.classList.toggle("link-list--open");
+    overlay.classList.toggle("overlay__active");
+});
